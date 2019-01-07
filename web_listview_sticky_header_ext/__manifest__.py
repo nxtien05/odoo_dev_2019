d305f923768be88b88b87cf344b7122404952b3d
nxtien05 <46451174+nxtien05@users.noreply.github.com>
2019-01-07 23:15:04 +0700

    Add files via upload

---
+++ web_listview_sticky_header_ext/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web List View Sticky Header Extend",
+
+    'summary': """
+      Web List View Sticky Header v12.0""",
+
+    'description': """
+        ext Fixed scrollLeft header
+    """,
+
+    'author': "Ksolves , Tien",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'version': '1.0.1',
+    'images': ['static/description/main.png'],
+    'depends': ['base'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+
+}
